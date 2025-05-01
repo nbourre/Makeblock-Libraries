92e034e1b161fe9c669fa18670cf4f02b631205e
Nick B <nbourrereg@hotmail.com>
2025-04-30 20:09:22 -0400

    Added documentation

--- src/MeAuriga.h
+++ src/MeAuriga.h
@@ -77,6 +77,8 @@
 #include "MePS2.h"
 #include "MePm25Sensor.h"
 #include "MeColorSensor.h"
+#include "MeRGBLineFollower.h"
+
 /*********************  Auriga Board GPIO Map *********************************/
 // struct defined in MeAuriga.h
  MePort_Sig mePort[17] =
